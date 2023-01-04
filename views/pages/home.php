a6400daebd65293da6a2f4fdb5a73a6deeceb49e
unknown <mauriaucourt.sarah@gmail.com>
2023-01-04 11:43:03 +0100

    fix registration

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,19 +1,21 @@
 <section>
     <div class="container">
-        <div class="p-5 jumbotron">
-            <h1 class="display-5 fw-bold">Bienvenu sur DesignFolio</h1>
+        <div class="p-5 jumbotron wow animate__fadeInDown">
+            <h1 class="display-5 fw-bold">Bienvenue sur DesignFolio</h1>
             <p class="col-md-10 fs-4">Plateforme où les créatifs du monde entier peuvent présenter leurs projets.
                 Développer votre image de marque personnelle et, si vous le faites correctement, vous pouvez attirer de
                 nouveaux clients.</p>
+            <?php if (!isset($_SESSION['id'])) { ?>
             <a href="index.php?action=registration" class="btn btn-primary btn-lg text-end" type="button">S'inscrire</a>
+            <?php } ?>
         </div>
     </div>
 </section>
 
 <section>
     <div class="container">
-        <div class="row g-3 mt-4">
-            <h2>Les derniers projets postés</h2>
+        <div class="row g-3 mt-4 wow animate__fadeInUp">
+            <h2 class="wow animate__bounceInLeft">Les derniers projets postés</h2>
             <?php
             $sql = "SELECT projet.*,user.login, user.avatar FROM projet JOIN user ON idAuteur=user.id ORDER BY projet.id DESC LIMIT 4 ";
             $req = $pdo->query($sql);
@@ -29,8 +31,8 @@
 
 <section>
     <div class="container">
-        <div class="row g-3 mt-4">
-            <h2>Le designer de la semaine</h2>
+        <div class="row g-3 mt-4 wow animate__fadeInUp">
+            <h2 class="wow animate__bounceInLeft">Le designer de la semaine</h2>
             <?php
             $sql = "SELECT projet.*,user.login, user.avatar FROM projet JOIN user ON 47=user.id ORDER BY projet.id ASC LIMIT 4 ";
             $req = $pdo->query($sql);
